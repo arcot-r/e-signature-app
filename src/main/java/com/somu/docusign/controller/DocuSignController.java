56c14ef58e18cdbb337f6a98709732ff03e8ee46
Rajanikanth Arcot <arcot.r@gmail.com>
2025-04-03 18:05:56 +0530

    added files

--- src/main/java/com/somu/docusign/controller/DocuSignController.java
+++ src/main/java/com/somu/docusign/controller/DocuSignController.java
@@ -1,4 +1,5 @@
 package com.somu.docusign.controller;
+
 import org.springframework.web.bind.annotation.*;
 
 import com.somu.docusign.service.DocuSignService;
@@ -9,19 +10,25 @@ import java.util.Map;
 @RequestMapping("/docusign")
 public class DocuSignController {
 
-    private final DocuSignService docuSignService;
+	private final DocuSignService docuSignService;
 
-    public DocuSignController(DocuSignService docuSignService) {
-        this.docuSignService = docuSignService;
-    }
+	public DocuSignController(DocuSignService docuSignService) {
+		this.docuSignService = docuSignService;
+	}
 
-    @PostMapping("/send")
-    public Map<String, String> sendDocument(@RequestParam String email, @RequestParam String name, @RequestParam String filePath) {
-        try {
-            String envelopeId = docuSignService.sendDocument(email, name, filePath);
-            return Map.of("envelopeId", envelopeId);
-        } catch (Exception e) {
-            return Map.of("error", e.getMessage());
-        }
-    }
+	@GetMapping("/hello")
+	public Map<String, String> hello() {
+		return Map.of("message", "Welcome to Docusign integration");
+	}
+
+	@PostMapping("/send")
+	public Map<String, String> sendDocument(@RequestParam String email, @RequestParam String name,
+			@RequestParam String filePath) {
+		try {
+			String envelopeId = docuSignService.sendDocument(email, name, filePath);
+			return Map.of("envelopeId", envelopeId);
+		} catch (Exception e) {
+			return Map.of("error", e.getMessage());
+		}
+	}
 }
